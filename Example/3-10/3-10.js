b2e00524a03bdab8144a46aa2cc9011cc0ccbf8d
zitachan303 <71454280+zitachan303@users.noreply.github.com>
2020-12-10 11:40:18 +0800

    thematic css change

--- Example/3-10/3-10.js
+++ Example/3-10/3-10.js
@@ -6,4 +6,8 @@ window.onload = function () {
 	var idEm = document.getElementById('no6');
 	idEm.innerHTML = 'No 6 changed!';
 	idEm.style.color = 'red';
+	idEm.style.backgroundColor = 'black';
+	//font-size =fontsize, text align = textAlign//
+	idEm.style.marginLeft = '5px';
+	
 }
---
+++ Example/3-11/3-11.js
@@ -0,0 +1,6 @@
+wondow.onload = function () {
+	var em = document.querySelector('article > p');
+	//em.style.color = 'red';
+	var ems = document.querySelectorAll('p');
+	ems[1].style.color = 'orange';
+};
